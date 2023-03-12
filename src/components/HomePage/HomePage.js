6b8e11569c08fb141b3d4a9b5f873cc8b32243b9
TRose2014 <shortyshort789@aol.com>
2023-03-12 17:53:55 +0900

    working on CORS issue to hit api from mongo

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -10,15 +10,49 @@ function HomePage() {
 
   useEffect(() => {
     async function getData() {
-      const url = 'https://remember-me-1-backend.herokuapp.com/info';
-      try {
-        const response = await axios.get(url);
-        console.log('response', response.data);
-        setPeople(response.data);
-        setLoading(false);
-      } catch (e) {
-        console.log(`There was an error ${e}`);
-      }
+      const data = JSON.stringify({
+        collection: 'people',
+        database: 'heroku_brk6g069',
+        dataSource: 'Remember-Me',
+        projection: {
+          _id: 1,
+        },
+      });
+
+      const config = {
+        method: 'post',
+        url: process.env.REACT_APP_API_URL,
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Access-Control-Request-Headers': '*',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+          'api-key': process.env.REACT_APP_API_KEY,
+        },
+        data,
+      };
+      // const url = process.env.REACT_APP_API_URL;
+      // console.log(`url ${url}`);
+
+      axios(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+          // response = JSON.stringify(response.data);
+          setPeople(JSON.stringify(response.data));
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      // try {
+      //   const response = await axios.get(url);
+      //   console.log('response', response.data);
+      //   setPeople(response.data);
+      //   setLoading(false);
+      // } catch (e) {
+      //   console.log(`There was an error ${e}`);
+      // }
     }
 
     getData();
