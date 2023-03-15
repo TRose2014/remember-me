62e95d7d340c75469ae0abc1cf1e35f79b20307a
TRose2014 <shortyshort789@aol.com>
2023-03-15 09:03:59 +0900

    converted db data to .json

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { CircularProgress } from '@material-ui/core';
-import axios from 'axios';
 import Typical from 'react-typical';
 import InfoCard from '../InfoCard/InfoCard';
 
@@ -10,47 +9,16 @@ function HomePage() {
 
   useEffect(() => {
     async function getData() {
-      const data = JSON.stringify({
-        collection: 'people',
-        database: 'heroku_brk6g069',
-        dataSource: 'Remember-Me',
-      });
-      const config = {
-        method: 'post',
-        // url: `https://crossorigin.me/${process.env.REACT_APP_API_URL}`,
-        url: `${process.env.REACT_APP_API_URL}`,
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-          'api-key': process.env.REACT_APP_API_KEY,
-        },
-        data,
-      };
 
-      axios(config)
-        .then((response) => {
-          console.log('Hello');
-          console.log(JSON.stringify(response.data));
-          // response = JSON.stringify(response.data);
-          setPeople(JSON.stringify(response.data));
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      try {
+        const response = await axios.get(url);
+        console.log('response', response.data);
+        setPeople(response.data);
+        setLoading(false);
+      } catch (e) {
+        console.log(`There was an error ${e}`);
+      }
 
-        // const url = `${process.env.REACT_APP_API_URL}`;
-        // console.log(`url ${url}`);
-
-      // try {
-      //   const response = await axios.get(url);
-      //   console.log('response', response.data);
-      //   setPeople(response.data);
-      //   setLoading(false);
-      // } catch (e) {
-      //   console.log(`There was an error ${e}`);
-      // }
     }
 
     getData();
